8a34c348c2db368b0c89271cd0b4678f27d3345f
Spencer Truett <srtruett1993@gmail.com>
2020-03-05 14:21:49 -0600

    First Commit

---
+++ Building.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Planner
+{
+  public class Building
+  {
+    private string _designer;
+    private DateTime _dateConstructed;
+    private string _address;
+    private string _owner;
+    public int Stories { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+    public double Volume
+    {
+      get
+      {
+        return 3 * Stories * Width * Depth;
+      }
+    }
+
+    // Methods
+    public void Construct()
+    {
+      _dateConstructed = DateTime.Now;
+    }
+    public void Purchase(string owner)
+    {
+      _owner = owner;
+
+    }
+
+    public void Description()
+    {
+      Console.WriteLine(this._address);
+      Console.WriteLine($"-------------------");
+      Console.WriteLine($"Designed by {this._designer}");
+      Console.WriteLine($"Constructed on {this._dateConstructed}");
+      Console.WriteLine($"Owned by {this._owner}");
+      Console.WriteLine($"{this.Volume} cubic meters of space");
+      Console.WriteLine("");
+    }
+
+    // Constructor
+    public Building(string address, double width, double depth, int stories)
+    {
+      _address = address;
+      Width = width;
+      Depth = depth;
+      Stories = stories;
+    }
+
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building FiveOneTwoEigth = new Building("512 8th Avenue", 1000, 500, 30);
+
+            FiveOneTwoEigth.Construct();
+            FiveOneTwoEigth.Purchase("Steve Brownlee");
+            FiveOneTwoEigth.Description();
+
+            Building ThreeSevenThreeSeven = new Building("37 37th Avenue", 800, 700, 10);
+
+            ThreeSevenThreeSeven.Construct();
+            ThreeSevenThreeSeven.Purchase("Mo Silvera");
+            ThreeSevenThreeSeven.Description();
+
+            Building OneTwoThreeFour = new Building("123 4th Avenue", 500, 500, 25);
+
+            OneTwoThreeFour.Construct();
+            OneTwoThreeFour.Purchase("Leah Hoefling");
+            OneTwoThreeFour.Description();
+
+        }
+    }
+}
